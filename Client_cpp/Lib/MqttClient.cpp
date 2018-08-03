ee40c982fc8005b6858d1d05d17dd34c95aa319a
user <tigrtolstoy@yandex.ru>
2018-08-03 14:36:40 +0300

    cpp lib

--- Client_cpp/Lib/MqttClient.cpp
+++ Client_cpp/Lib/MqttClient.cpp
@@ -37,30 +37,36 @@ void mqttClient::init_client_loop()
 void mqttClient::subscribe(int platform_id)
 {
     char topic[64];
-    sprintf(topic, "esp32/%d/", platform_id);
+    char theme[64];
+    sprintf(theme, "%d", platform_id);
+    sprintf(topic, "esp32/%d", platform_id);
     //Loop until we're reconnected
-    while (!client->connected()) {
+    while (!client->connected())
+    {
         // Attempt to connect
-        if (client->connect("ESP8266Client")) {
-        // Subscribe to topic
-        client->subscribe(topic);
-        } else {
-        // Wait 0.5 seconds before retrying
-        delay(500);
+        if (client->connect("ESP8266Client"))
+        {
+            // Subscribe to topic
+            client->subscribe(topic);
+            client->publish("Connected", theme);
+        }
+        else
+        {
+            // Wait 0.5 seconds before retrying
+            delay(500);
         }
     }
 }
 
-void mqttClient::pub_msg(char* msg, int platform_id)
+
+void mqttClient::pub_feedback(char* msg, int platform_id)
 {
     char theme[64];
     char topic[64];
-    sprintf(topic, "esp32/feedback/%d/", platform_id);
+    sprintf(topic, "esp32/feedback/%d", platform_id);
     sprintf(theme, "Message <<%s>> is received!",msg);
     client->publish(topic, theme);
 }
-
-
 
 void mqttClient::set_callback(void (*callback)(char* topic, byte* message, unsigned int length))
 {
